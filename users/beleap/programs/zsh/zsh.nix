5c3219b0035aaea9cf84a7b725f794d303d54de7
BeLeap <beleap@beleap.dev>
2025-07-20 17:40:47 +0900

    feat: add some alias & option

--- users/beleap/programs/zsh/zsh.nix
+++ users/beleap/programs/zsh/zsh.nix
@@ -2,10 +2,14 @@ _: {
   programs.zsh = {
     enable = true;
 
+    autocd = true;
+
     shellAliases = {
       ga = "git add";
       gc = "git commit -v";
       gst = "git status";
+
+      sozsh = "source ~/.zshrc";
     };
   };
 }
