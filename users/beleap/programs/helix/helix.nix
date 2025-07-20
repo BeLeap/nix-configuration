23d2ffe17d9df775e35a0c45c3bc3bccde44056b
BeLeap <beleap@beleap.dev>
2025-07-20 15:51:13 +0900

    feat: helix config with nix

--- users/beleap/programs/helix/helix.nix
+++ users/beleap/programs/helix/helix.nix
@@ -1,5 +1,43 @@
 _: {
   programs.helix = {
     enable = true;
+
+    settings = {
+      theme = "catppuccin_frappe";
+
+      editor = {
+        line-number = "relative";
+        end-of-line-diagnostics = "hint";
+
+        inline-diagnostics = {
+          cursor-line = "error";
+        };
+
+        file-picker = {
+          hidden = false;
+          git-ignore = false;
+          git-global = false;
+          git-exclued = false;
+        };
+
+        soft-wrap = {
+          enable = true;
+        };
+
+        status-line = {
+          left = ["mode" "spinner"];
+          center = ["file-name" "read-only-indicator" "file-modification-indicator"];
+          right = ["diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type"];
+        };
+
+        lsp = {
+          display-messages = true;
+          display-inlay-hints = true;
+        };
+
+        whitespace.render.tab = "all";
+        editor.indent-guides.render = true;
+      };
+    };
   };
 }
