6e96c761c5e23f73e2168cbb6312d69ec0a127a9
BeLeap <beleap@beleap.dev>
2025-07-20 16:07:00 +0900

    feat: helix language settings

--- users/beleap/programs/helix/helix.nix
+++ users/beleap/programs/helix/helix.nix
@@ -39,5 +39,68 @@ _: {
         indent-guides.render = true;
       };
     };
+
+    languages = {
+      language-server.biome = {
+        command = "biome";
+        args = ["lsp-proxy"];
+        required-root-patterns = ["biome.json"];
+      };
+      language-server.kotlin-ls = {
+        command = "kotlin-ls";
+        args = ["--stdio"];
+        timeout = 60;
+        required-root-pattern = ["build.gradle" "build.gradle.kts" "pom.xml"];
+      };
+
+      language = [
+        {
+          name = "rust";
+          auto-format = true;
+          language-servers = ["rust-analyzer"];
+        }
+        {
+          name = "python";
+          language-servers = ["pyright"];
+        }
+        {
+          name = "dockerfile";
+          scope = "source.dockerfile";
+          file-types = ["Dockerfile" { glob = "*Dockerfile*"; }];
+        }
+        {
+          name = "bash";
+          indent = { tab-width = 4; unit = "    "; };
+          formatter = { command = "shfmt"; args = ["-i" "4"]; };
+          auto-format = true;
+        }
+        {
+          name = "kotlin";
+          scope = "source.kotlin";
+          injection-regex = "kotlin";
+          file-types = ["kt" "kts"];
+          roots = ["build.gradle" "build.gradle.kts" "settings.gradle" "settings.gradle.kts" "pom.xml"];
+          auto-format = true;
+          language-servers = ["kotlin-ls"];
+        }
+      ]
+      ++
+      # Javascript family
+      map (name: {
+        name = name;
+        auto-format = true;
+        language-servers = [
+          { name = "typescrpit-language-server"; except-featuers = ["format"]; }
+          "biome"
+        ];
+      }) ["javascript" "typescript" "jsx" "tsx" "json"];
+
+      grammar = [
+        {
+          name = "dockerfile";
+          source = { git = "https://github.com/camdencheek/tree-sitter-dockerfile"; rev = "main"; };
+        }
+      ];
+    };
   };
 }
