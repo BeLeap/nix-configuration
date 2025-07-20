35400fadf56713caded3434d0fe38e1ff5b908e2
BeLeap <beleap@beleap.dev>
2025-07-20 17:36:00 +0900

    feat: use preset

--- users/beleap/programs/starship/starship.nix
+++ users/beleap/programs/starship/starship.nix
@@ -1,7 +1,12 @@
-_: {
+{ pkgs, ... }:
+{
   programs.starship = {
     enable = true;
 
     enableZshIntegration = true;
+
+    settings =
+      with builtins;
+      (fromTOML (readFile "${pkgs.starship}/share/starship/presets/bracketed-segments.toml"));
   };
 }
