997ef7fe5f69444527c7b24a53fab4e38adf0541
BeLeap <beleap@beleap.dev>
2025-07-20 17:11:23 +0900

    fix: nested

--- users/beleap/programs/ghostty/ghostty.nix
+++ users/beleap/programs/ghostty/ghostty.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }:
 {
-  # Installed using brew on macOS
-  programs.ghostty.enable = !pkgs.stdenv.isDarwin;
+  programs.ghostty = {
+    # Installed using brew on macOS
+    enable = !pkgs.stdenv.isDarwin;
 
-  settings = {
-    theme = "catppuccin-frappe";
+    settings = {
+      theme = "catppuccin-frappe";
 
-    font-family = "Cascadia Code NF";
-    font-feature = "'calt', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09', 'liga'";
-    font-size = 18;
+      font-family = "Cascadia Code NF";
+      font-feature = "'calt', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09', 'liga'";
+      font-size = 18;
 
-    window-padding-x = 10;
-    window-title-font-family = "Cascadia Code NF";
-    # Clamped to screen size so below works as maxmized
-    window-height = 99999;
-    window-width = 99999;
+      window-padding-x = 10;
+      window-title-font-family = "Cascadia Code NF";
+      # Clamped to screen size so below works as maxmized
+      window-height = 99999;
+      window-width = 99999;
 
-    shell-integration-features = "no-title";
-
+      shell-integration-features = "no-title";
+    };
   };
 }
