4d4055e2511c79092b225cdb11911d7b770a5a0c
BeLeap <beleap@beleap.dev>
2025-07-20 14:54:50 +0900

    feat: set user

--- users/beleap/darwin.nix
+++ users/beleap/darwin.nix
@@ -2,7 +2,18 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
   home.stateVersion = "25.11";
+
+  home.username = "beleap";
+  home.homeDirectory = "/Users/beleap";
 
   home.packages = [
     pkgs.htop
