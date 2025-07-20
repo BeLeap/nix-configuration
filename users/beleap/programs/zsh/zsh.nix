6d178d8bcffd0bae5e398cbe7cdf0ee8b44f1040
BeLeap <beleap@beleap.dev>
2025-07-20 17:47:20 +0900

    feat: add some zsh opt

--- users/beleap/programs/zsh/zsh.nix
+++ users/beleap/programs/zsh/zsh.nix
@@ -11,6 +11,25 @@ _: {
       enable = true;
     };
 
+    initContent = ''
+      # Safety
+      setopt noclobber nomatch
+
+      # Directory convenience
+      setopt autocd chase_links
+      setopt pushd_ignore_dups pushd_silent pushd_to_home
+
+      # History behavior
+      setopt share_history inc_append_history
+      setopt hist_ignore_dups hist_ignore_space
+
+      # Globbing
+      setopt extended_glob
+
+      # Spell-check
+      setopt correct_all
+    '';
+
     shellAliases = {
       ga = "git add";
       gc = "git commit -v";
