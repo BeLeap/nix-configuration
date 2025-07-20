892ef0fe42e6d181dc38f7be4dc5fd0c503cf015
BeLeap <beleap@beleap.dev>
2025-07-20 16:52:03 +0900

    refact: separate editor config

---
+++ users/beleap/programs/helix/editor.nix
@@ -0,0 +1,33 @@
+{
+  line-number = "relative";
+  end-of-line-diagnostics = "hint";
+
+  inline-diagnostics = {
+    cursor-line = "error";
+  };
+
+  file-picker = {
+    hidden = false;
+    git-ignore = false;
+    git-global = false;
+    git-exclude = false;
+  };
+
+  soft-wrap = {
+    enable = true;
+  };
+
+  statusline = {
+    left = ["mode" "spinner"];
+    center = ["file-name" "read-only-indicator" "file-modification-indicator"];
+    right = ["diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type"];
+  };
+
+  lsp = {
+    display-messages = true;
+    display-inlay-hints = true;
+  };
+
+  whitespace.render.tab = "all";
+  indent-guides.render = true;
+}
--- users/beleap/programs/helix/helix.nix
+++ users/beleap/programs/helix/helix.nix
@@ -7,39 +7,7 @@
     settings = {
       theme = "catppuccin_frappe";
 
-      editor = {
-        line-number = "relative";
-        end-of-line-diagnostics = "hint";
-
-        inline-diagnostics = {
-          cursor-line = "error";
-        };
-
-        file-picker = {
-          hidden = false;
-          git-ignore = false;
-          git-global = false;
-          git-exclude = false;
-        };
-
-        soft-wrap = {
-          enable = true;
-        };
-
-        statusline = {
-          left = ["mode" "spinner"];
-          center = ["file-name" "read-only-indicator" "file-modification-indicator"];
-          right = ["diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type"];
-        };
-
-        lsp = {
-          display-messages = true;
-          display-inlay-hints = true;
-        };
-
-        whitespace.render.tab = "all";
-        indent-guides.render = true;
-      };
+      editor = import ./editor.nix;
     };
 
     languages = {
