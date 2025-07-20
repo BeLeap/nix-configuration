de63127fe2cbdc766a7341ff092dda54ec7cc541
BeLeap <beleap@beleap.dev>
2025-07-20 15:25:24 +0900

    feat: run nix gc automatically

--- configurations/common/common.nix
+++ configurations/common/common.nix
@@ -1,4 +1,15 @@
 _:
 {
   nix.optimise.automatic = true;
+  nix.gc = {
+    automatic = true;
+    interval = [
+      {
+        Hour = 3;
+        Minute = 15;
+        Weekday = 7;
+      }
+    ];
+    options = "--delete-older-than 3d";
+  };
 }
