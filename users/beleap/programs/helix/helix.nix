7a590a54cc2461a0e93937949c465ee4c05b3414
BeLeap <beleap@beleap.dev>
2025-07-20 16:47:22 +0900

    refact: separate languages

--- users/beleap/programs/helix/helix.nix
+++ users/beleap/programs/helix/helix.nix
@@ -59,47 +59,7 @@
         args = [ "--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib" ];
       };
 
-      language = [
-        {
-          name = "rust";
-          auto-format = true;
-          language-servers = ["rust-analyzer"];
-        }
-        {
-          name = "python";
-          language-servers = ["pyright"];
-        }
-        {
-          name = "dockerfile";
-          scope = "source.dockerfile";
-          file-types = ["Dockerfile" { glob = "*Dockerfile*"; }];
-        }
-        {
-          name = "bash";
-          indent = { tab-width = 4; unit = "    ";};
-          formatter = { command = "shfmt"; args = ["-i" "4"]; };
-          auto-format = true;
-        }
-        {
-          name = "kotlin";
-          scope = "source.kotlin";
-          injection-regex = "kotlin";
-          file-types = ["kt" "kts"];
-          roots = ["build.gradle" "build.gradle.kts" "settings.gradle" "settings.gradle.kts" "pom.xml"];
-          auto-format = true;
-          language-servers = ["kotlin-ls"];
-        }
-      ]
-      ++
-      # Javascript family
-      map (name: {
-        name = name;
-        auto-format = true;
-        language-servers = [
-          { name = "typescript-language-server"; except-features = ["format"]; }
-          "biome"
-        ];
-      }) ["javascript" "typescript" "jsx" "tsx" "json"];
+      language = import ./languages.nix;
     };
   };
 }
