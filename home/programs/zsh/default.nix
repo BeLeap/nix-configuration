db26d8337dee0c05b8aff69966e615e8048cd40b
BeLeap <beleap@beleap.dev>
2025-07-22 00:57:45 +0900

    feat: add git alias

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -44,6 +44,7 @@ _: {
       ggr = "cd $(git rev-parse --show-toplevel 2>/dev/null)";
       gl = "git pull --rebase";
       gr = "git rebase --autostash --autosquash";
+      gsw = "git switch";
 
       prcm = "gh pr create --assignee @me";
       prv = "gh pr view";
