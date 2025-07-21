1ce7a7d2728b2223f8f2309f66c57a0fbc72c383
BeLeap <beleap@beleap.dev>
2025-07-22 00:07:57 +0900

    feat: add fzf

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -30,8 +30,6 @@ _: {
       setopt correct_all
 
       bindkey -v
-      bindkey \^R history-incremental-search-backward
-      bindkey \^S history-incremental-search-forward
 
       export PATH=/run/current-system/sw/bin:$PATH
     '';
