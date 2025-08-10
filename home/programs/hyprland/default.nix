3d2aaa353179ff219ff25870f195a8534a7c21f9
BeLeap <beleap@beleap.dev>
2025-08-10 17:27:12 +0900

    run foot

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -1,5 +1,14 @@
-_: {
+{ pkgs, ... }:
+{
   wayland.windowManager.hyprland = {
     enable = true;
+
+    settings = {
+      "$mod" = "SUPER";
+
+      bind = [
+        "$mod, x, exec, ${pkgs.foot}/bin/foot"
+      ];
+    };
   };
 }
