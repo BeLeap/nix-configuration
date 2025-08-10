148b4a4d262431d5499ee0fa66641bfd7aabe2a3
BeLeap <beleap@beleap.dev>
2025-08-10 21:55:22 +0900

    run alacritty on run terminal

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -14,7 +14,7 @@
 
       bind = [
         "$mod, x, exec, ${pkgs.foot}/bin/foot"
-        "$mod, t, exec, ${pkgs.ghostty}/bin/ghostty"
+        "$mod, t, exec, ${pkgs.alacritty}/bin/alacritty"
         "$mod, r, exec, ${pkgs.rofi}/bin/rofi -show drun"
       ];
 
