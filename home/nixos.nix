c15a877b9e4c28d8c3d6434940583213cb6ddec7
BeLeap <beleap@beleap.dev>
2025-08-10 17:17:03 +0900

    fix hyprland setting

--- home/nixos.nix
+++ home/nixos.nix
@@ -2,14 +2,13 @@
 {
   imports = [
     ./common.nix
-  ]
-  ++ map (p: (./. + "/programs/${p}")) [
-    "hyprland"
   ];
 
   home.homeDirectory = "/home/${metadata.usernameLower}";
 
   wayland = {
-    windowManager.hyprland.enable = true;
+    windowManager.hyprland = {
+      enable = true;
+    };
   };
 }
