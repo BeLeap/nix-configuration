dd3d75f9a38f9eef8c73e10d83b6a828ccdee26a
BeLeap <beleap@beleap.dev>
2025-08-10 22:05:13 +0900

    remove ghostty

--- configurations/macos/common/brew.nix
+++ configurations/macos/common/brew.nix
@@ -4,11 +4,6 @@
     enable = true;
 
     brews = [ ];
-    casks = [
-      # nixpkgs darwin ghostty is broken
-      # See https://github.com/ghostty-org/ghostty/discussions/4359
-      "ghostty"
-    ]
-    ++ (lib.optionals (metadata.kind == "personal") [ "logseq" ]);
+    casks = [ ] ++ (lib.optionals (metadata.kind == "personal") [ "logseq" ]);
   };
 }
