162c91044b3c25f3721d56071a98cbb96d274474
BeLeap <beleap@beleap.dev>
2025-08-10 17:21:31 +0900

    extract file

--- home/nixos.nix
+++ home/nixos.nix
@@ -2,13 +2,10 @@
 {
   imports = [
     ./common.nix
+  ]
+  ++ map (p: (./. + "/programs/${p}")) [
+    "hyprland"
   ];
 
   home.homeDirectory = "/home/${metadata.usernameLower}";
-
-  wayland = {
-    windowManager.hyprland = {
-      enable = true;
-    };
-  };
 }
