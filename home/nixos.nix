9e84a42b199d346435b1c7844bd261c3b998af03
BeLeap <beleap@beleap.dev>
2025-08-10 21:52:13 +0900

    add alacritty

--- home/nixos.nix
+++ home/nixos.nix
@@ -1,10 +1,14 @@
-{ metadata, ... }:
+{ metadata, pkgs, ... }:
 {
   imports = [
     ./common.nix
   ]
   ++ map (p: (./. + "/programs/${p}")) [
     "hyprland"
+  ];
+
+  home.packages = with pkgs; [
+    alacritty
   ];
 
   home.homeDirectory = "/home/${metadata.usernameLower}";
