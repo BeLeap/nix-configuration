b099d053fca9b4fa0a85d9aea39e83cf1aad2261
BeLeap <beleap@beleap.dev>
2025-08-16 14:40:43 +0900

    temp patch colemak

--- home/programs/aerospace/default.nix
+++ home/programs/aerospace/default.nix
@@ -8,9 +8,54 @@ _: {
     };
 
     userSettings = {
+      # NOTE: use colemak after home-manager fixes
       key-mapping = {
-        # NOTE: use colemak after home-manager fixes
-        preset = "dvorak";
+        preset = "qwerty";
+      };
+      key-mapping.key-notation-to-key-code = {
+        q = "q";
+        w = "w";
+        f = "e";
+        p = "r";
+        g = "t";
+        j = "y";
+        l = "u";
+        u = "i";
+        y = "o";
+        semicolon = "p";
+        leftSquareBracket = "leftSquareBracket";
+        rightSquareBracket = "rightSquareBracket";
+        backslash = "backslash";
+
+        a = "a";
+        r = "s";
+        s = "d";
+        t = "f";
+        d = "g";
+        h = "h";
+        n = "j";
+        e = "k";
+        i = "l";
+        o = "semicolon";
+        quote = "quote";
+
+        z = "z";
+        x = "x";
+        c = "c";
+        v = "v";
+        b = "b";
+        k = "n";
+        m = "m";
+        comma = "comma";
+        period = "period";
+        slash = "slash";
+      };
+
+      mode.main.binding = {
+        alt-h = "focus left";
+        alt-j = "focus down";
+        alt-k = "focus up";
+        alt-l = "focus right";
       };
     };
   };
