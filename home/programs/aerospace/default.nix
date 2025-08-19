7a7fa51b5a46e886dab18d0dbb812d24c9fcde6a
BeLeap <beleap@beleap.dev>
2025-08-19 14:30:47 +0900

    use colemak

--- home/programs/aerospace/default.nix
+++ home/programs/aerospace/default.nix
@@ -9,6 +9,10 @@
     };
 
     userSettings = {
+      key-mapping = {
+        preset = "colemak";
+      };
+
       exec-on-workspace-change = [
         "/bin/bash"
         "-c"
@@ -79,50 +83,6 @@
           run = [ "move-node-to-workspace 1" ];
         }
       ];
-    }
-    # NOTE: use colemak after home-manager fixes
-    // {
-      key-mapping = {
-        preset = "qwerty";
-      };
-      key-mapping.key-notation-to-key-code = {
-        q = "q";
-        w = "w";
-        f = "e";
-        p = "r";
-        g = "t";
-        j = "y";
-        l = "u";
-        u = "i";
-        y = "o";
-        semicolon = "p";
-        leftSquareBracket = "leftSquareBracket";
-        rightSquareBracket = "rightSquareBracket";
-        backslash = "backslash";
-
-        a = "a";
-        r = "s";
-        s = "d";
-        t = "f";
-        d = "g";
-        h = "h";
-        n = "j";
-        e = "k";
-        i = "l";
-        o = "semicolon";
-        quote = "quote";
-
-        z = "z";
-        x = "x";
-        c = "c";
-        v = "v";
-        b = "b";
-        k = "n";
-        m = "m";
-        comma = "comma";
-        period = "period";
-        slash = "slash";
-      };
     };
   };
 }
