5e250540bd24f0851cb8a19e96b64fe44a4f4f23
BeLeap <beleap@beleap.dev>
2025-08-24 12:16:01 +0900

    update config

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -7,9 +7,20 @@ _: {
 
     extraConfig = ''
       return {
-        font = wezterm.font("Cascadia Code NF"),
+        font = wezterm.font_with_fallback {
+          {
+            family = "Cascadia Code NF"
+          },
+          {
+            family = "NanumGothicCoding"
+          },
+        },
         font_size = 16.0,
         color_scheme = "Catppuccin Frappe",
+
+        window_frame = {
+          font = wezterm.font("Cascadia Code NF"),
+        },
       }
     '';
   };
