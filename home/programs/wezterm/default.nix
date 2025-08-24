0299cb9745505860eaf2867bdf72b5e9ba6fb941
BeLeap <beleap@beleap.dev>
2025-08-24 12:09:27 +0900

    add config

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -4,5 +4,13 @@ _: {
 
     enableBashIntegration = true;
     enableZshIntegration = true;
+
+    extraConfig = ''
+      return {
+        font = wezterm.font("Cascadia Code NF"),
+        font_size = 16.0,
+        color_scheme = "Catppuccin Frappe",
+      }
+    '';
   };
 }
