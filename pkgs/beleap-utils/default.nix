e65a4f98b8f358af3db25502ae03583eb3d68791
BeLeap <beleap@beleap.dev>
2025-08-25 00:25:34 +0900

    fix commit prefix

--- pkgs/beleap-utils/default.nix
+++ pkgs/beleap-utils/default.nix
@@ -6,5 +6,7 @@ pkgs.symlinkJoin {
     (pkgs.writeShellScriptBin "crcpr" (builtins.readFile ./crcpr))
     (pkgs.writeShellScriptBin "fco" (builtins.readFile ./fco))
     (pkgs.writeShellScriptBin "fcd" (builtins.readFile ./fcd))
+    (pkgs.writeShellScriptBin "jjcp" (builtins.readFile ./jjcp))
+    (pkgs.writeShellScriptBin "gh-issue" (builtins.readFile ./gh-issue))
   ];
 }
