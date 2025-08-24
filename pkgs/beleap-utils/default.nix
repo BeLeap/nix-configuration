93dbce7c533f691d6c2fdcdd8e81e884219b310e
BeLeap <beleap@beleap.dev>
2025-08-25 00:46:09 +0900

    update scripts

--- pkgs/beleap-utils/default.nix
+++ pkgs/beleap-utils/default.nix
@@ -2,12 +2,7 @@ pkgs:
 
 pkgs.symlinkJoin {
   name = "beleap-utils";
-  paths = [
-    (pkgs.writeShellScriptBin "crcpr" (builtins.readFile ./crcpr))
-    (pkgs.writeShellScriptBin "fco" (builtins.readFile ./fco))
-    (pkgs.writeShellScriptBin "fcd" (builtins.readFile ./fcd))
-    (pkgs.writeShellScriptBin "jjcp" (builtins.readFile ./jjcp))
-    (pkgs.writeShellScriptBin "gh-issue" (builtins.readFile ./gh-issue))
-    (pkgs.writeShellScriptBin "jjsb" (builtins.readFile ./jjsb))
-  ];
+  paths = builtins.map (
+    file: pkgs.writeShellScriptBin file (builtins.readFile (./. + "/scripts/${file}"))
+  ) (builtins.attrNames (builtins.readDir ./scripts));
 }
