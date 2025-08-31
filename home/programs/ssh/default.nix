984a8a4f390600a374a5e2ace9a1a9882d174d40
BeLeap <beleap@beleap.dev>
2025-09-01 00:33:43 +0900

    use 1password only with personal setup

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -1,12 +1,20 @@
-_: {
+{ metadata, ... }:
+{
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks = {
-      "*" = {
-        identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-      };
-    };
+    matchBlocks =
+      { }
+      // (
+        if (metadata.kind == "personal") then
+          {
+            "*" = {
+              identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+            };
+          }
+        else
+          { }
+      );
   };
 }
