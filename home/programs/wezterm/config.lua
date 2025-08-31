e07b37df29e647df623608202196e5d6b09e300b
BeLeap <beleap@beleap.dev>
2025-09-01 00:25:30 +0900

    update wezterm config

--- home/programs/wezterm/config.lua
+++ home/programs/wezterm/config.lua
@@ -1,27 +1,29 @@
-return {
-  font = wezterm.font_with_fallback {
-    {
-      family = "Cascadia Code NF"
-    },
-    {
-      family = "NanumGothicCoding"
-    },
-  },
-  font_size = 16.0,
-  color_scheme = "Catppuccin Frappe",
+local config = {};
 
-  window_frame = {
-    font = wezterm.font("Cascadia Code NF"),
-  },
-  window_decorations = "RESIZE",
+config.font = wezterm.font_with_fallback {
+  { family = "Cascadia Code NF" },
+  { family = "NanumGothicCoding" },
+};
+config.font_size = 16.0;
+config.window_frame = {
+  font = wezterm.font("Cascadia Code NF"),
+};
 
-  leader = { key = 'a', mods = 'CTRL', timout_milliseconds = 1000 },
-  keys = {
-    { key = '%', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-    { key = '"', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
-    { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
-    { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
-    { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
-    { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
-  };
-}
+config.color_scheme = "Catppuccin Frappe";
+config.window_decorations = "RESIZE";
+
+config.leader = { key = 'a', mods = 'CTRL', timout_milliseconds = 1000 };
+config.keys = {
+   -- Double tap CTRL-a to send CTRL-a to terminal
+  { key = 'a', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' } },
+
+  { key = '%', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = '"', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+
+  { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
+};
+
+return config;
