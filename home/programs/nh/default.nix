298bff0e9adbd3d59e0350a1ce2ca9f1f296ced5
BeLeap <beleap@beleap.dev>
2025-10-06 14:30:14 +0900

    setup nh

---
+++ home/programs/nh/default.nix
@@ -0,0 +1,18 @@
+{ metadata, ... }:
+{
+  programs.nh = {
+    enable = true;
+    flake =
+      (if (metadata.os == "darwin") then "/Users" else "/home")
+      + "/"
+      + metadata.usernameLower
+      + "/nix-configuration#"
+      + (if (metadata.os == "darwin") then "darwinConfigurations" else "nixosConfigurations ")
+      + "."
+      + metadata.name;
+
+    clean = {
+      enable = true;
+    };
+  };
+}
