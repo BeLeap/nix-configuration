4c62cf775be4fe6963f7bc879624843556250efc
BeLeap <beleap@beleap.dev>
2025-10-09 22:59:08 +0900

    support arrow

--- home/programs/wezterm/config.lua
+++ home/programs/wezterm/config.lua
@@ -30,9 +30,13 @@ config.keys = {
   { key = 'q', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true } },
 
   { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
+  { key = 'LeftArrow', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
   { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
+  { key = 'DownArrow', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
   { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
+  { key = 'UpArrow', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
   { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
+  { key = 'RightArrow', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
 
   { key = '[', mods = 'LEADER', action = act.ActivateCopyMode },
   { key = ']', mods = 'LEADER', action = act.PasteFrom('PrimarySelection') },
