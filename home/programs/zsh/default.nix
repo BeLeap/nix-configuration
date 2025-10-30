442da8f055e9376835d458e30ddffc112bc7e23d
BeLeap <beleap@beleap.dev>
2025-10-30 12:55:51 +0900

    adjust indent

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -32,9 +32,9 @@ _: {
       bindkey -v
 
       fcd() {
-          local file
-          local dir
-          file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
+        local file
+        local dir
+        file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
       }
     '';
 
