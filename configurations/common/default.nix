49cc6f723ffd2292f9c8d27152393a01c3b315a2
BeLeap <beleap@beleap.dev>
2025-12-06 13:37:19 +0900

    minor fix

--- configurations/common/default.nix
+++ configurations/common/default.nix
@@ -10,7 +10,6 @@
   environment.shells = [ pkgs.zsh ];
 
   programs.ssh = {
-    enableDefaultConfig = false;
     knownHosts = {
       "github.com".publicKey =
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
