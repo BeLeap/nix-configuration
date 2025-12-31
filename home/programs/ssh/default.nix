8e381b30ba0cd8f900a2caf78ace78be72824369
BeLeap <beleap@beleap.dev>
2025-12-31 21:13:08 +0900

    separate gui recipe

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -4,19 +4,18 @@
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks =
-      {
-        "*" = { };
-      }
-      // (
-        if (metadata.kind == "personal" && metadata.gui) then
-          {
-            "*" = {
-              identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-            };
-          }
-        else
-          { }
-      );
+    matchBlocks = {
+      "*" = { };
+    }
+    // (
+      if (metadata.kind == "personal" && metadata.gui) then
+        {
+          "*" = {
+            identityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+          };
+        }
+      else
+        { }
+    );
   };
 }
