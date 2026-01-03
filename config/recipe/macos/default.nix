b5cc5cb3602059cc2cff3efb7ebe1494aaa921ba
Changseo Jang <beleap@beleap.dev>
2026-01-04 05:50:15 +0900

    refactor: move host configs into recipe modules (#10)
    * refactor: move host configs into recipes
    
    * fix: inputs

--- config/recipe/macos/default.nix
+++ config/recipe/macos/default.nix
@@ -1,8 +1,23 @@
-{ lib, metadata }:
+{
+  lib,
+  metadata,
+}:
 (lib.optionalAttrs (metadata.distribution == "macos") {
   base =
-    { pkgs, ... }:
+    { pkgs, inputs, ... }:
     {
+      environment.systemPackages = [ ];
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
       homebrew = {
         enable = !(metadata.kind == "airgap");
 
--- mkSystem.nix
+++ mkSystem.nix
@@ -20,9 +20,7 @@ inputs@{
 let
   # TODO: we could remove metadata from specialArgs after migration to configs finishes
   specialArgs = { inherit inputs metadata; };
-  modules = (import ./config { inherit inputs recipes; }) ++ [
-    (./. + "/configurations/${metadata.distribution}/${metadata.configPath}/configuration.nix")
-  ];
+  modules = import ./config { inherit inputs recipes; };
 in
 {
   nixosConfigurations = lib.optionalAttrs (metadata.distribution == "nixos") {
