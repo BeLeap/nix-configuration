beffc4bc8682b70a0b3dcd969e994177c4fa88ec
Changseo Jang <beleap@beleap.dev>
2026-01-31 16:50:36 +0900

    Move development home settings into recipe

--- config/recipe/development/default.nix
+++ config/recipe/development/default.nix
@@ -1,11 +1,126 @@
 _: {
   hm = [
     (
-      { pkgs, ... }:
       {
-        home.packages = with pkgs; [
-          just
-        ];
+        pkgs,
+        lib,
+        metadata,
+        ...
+      }:
+      {
+        home.packages =
+          with pkgs;
+          [
+            htop
+            wireshark
+            cascadia-code
+            nanum-gothic-coding
+            ipcalc
+            mtr
+            arping
+            mitmproxy
+            ncdu
+            yq-go
+            watchexec
+            beleap-utils
+            gnupg
+            oauth2c
+            curl
+            fzf-make
+            boda
+            ghc
+            yaml-language-server
+            empiriqa
+            gnumake
+            croc
+            dnsi
+            crane
+            coreutils-full
+            gtrash
+            dive
+            # pgcli
+            # mycli
+            ssm-session-manager-plugin
+            awscli2
+            just
+          ]
+          ++ (lib.optionals (metadata.kind == "personal") [
+            pkgs._1password-cli
+          ])
+          # Discord packages is not supported on aarch64-linux
+          ++ (lib.optionals (metadata.kind == "personal" && (metadata.platform == "x86_64-linux")) [
+            pkgs.discord
+          ])
+          ++ (lib.optionals (metadata.kind == "work") (
+            with pkgs;
+            [
+              unstable.jira-cli-go
+            ]
+          ));
+
+        imports =
+          [ ]
+          ++ map (p: (./. + "/../../../home/programs/${p}")) (
+            [
+              "zsh"
+              "lsd"
+              "starship"
+              "carapace"
+              "helix"
+              "zoxide"
+              "direnv"
+              "git"
+              "fzf"
+              "gh"
+              "bash"
+              "jujutsu"
+              "ssh"
+              "nh"
+              "podman"
+              "codex"
+              "claude-code"
+            ]
+            ++ (lib.optionals (metadata.gui) [
+              "firefox"
+              "wezterm"
+            ])
+          );
+
+        home.shellAliases = {
+          ga = "git add";
+          gc = "git commit -v";
+          gst = "git status";
+          gp = "git push";
+          gf = "git fetch --all --prune";
+          gd = "git diff";
+          ggr = "cd $(git rev-parse --show-toplevel 2>/dev/null)";
+          gl = "git pull --rebase";
+          gr = "git rebase --autostash --autosquash";
+          gsw = "git switch";
+          glp = "git pull --rebase && git push";
+
+          prcm = "gh pr create --assignee @me";
+          prv = "gh pr view";
+          prvw = "gh pr view -w";
+          prm = "gh pr merge";
+          prmd = "gh pr merge -d";
+
+          jgf = "jj git fetch --all-remotes";
+          jgp = "jj git push";
+
+          e = "$EDITOR";
+
+          docker = "podman";
+
+          rm = "echo Use the full path i.e. '/bin/rm', consider using trash";
+          tp = "gtrash p";
+          fm = "fzf-make";
+        };
+
+        home.sessionVariables = {
+          EDITOR = "hx";
+          MAKEFLAGS = "-j$(nproc)";
+        };
       }
     )
   ];
