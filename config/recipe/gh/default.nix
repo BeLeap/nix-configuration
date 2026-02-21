7a378bcf38147c96307ccb57d8da19738e3b8f0f
Changseo Jang <beleap@beleap.dev>
2026-02-21 11:58:35 +0900

    Move tool aliases to recipes (#46)
    * move jujutsu related
    
    * move git
    
    * move gh
    
    * move podman

--- config/recipe/gh/default.nix
+++ config/recipe/gh/default.nix
@@ -2,6 +2,13 @@ _: {
   hm = [
     (
       _: {
+        home.shellAliases = {
+          prcm = "gh pr create --assignee @me";
+          prv = "gh pr view";
+          prvw = "gh pr view -w";
+          prm = "gh pr merge";
+          prmd = "gh pr merge -d";
+        };
         programs.gh = {
           enable = true;
         };
--- config/recipe/git/default.nix
+++ config/recipe/git/default.nix
@@ -2,6 +2,19 @@ _: {
   hm = [
     (
       {metadata, ...}: {
+        home.shellAliases = {
+          ga = "git add";
+          gc = "git commit -v";
+          gst = "git status";
+          gp = "git push";
+          gf = "git fetch --all --prune";
+          gd = "git diff";
+          ggr = "cd $(git rev-parse --show-toplevel 2>/dev/null)";
+          gl = "git pull --rebase";
+          gr = "git rebase --autostash --autosquash";
+          gsw = "git switch";
+          glp = "git pull --rebase && git push";
+        };
         programs.git = {
           enable = true;
 
