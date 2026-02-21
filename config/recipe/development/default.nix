0d143161dd35f314b6d54dfa497360a26d733132
BeLeap <beleap@beleap.dev>
2026-02-21 11:36:16 +0900

    move git

--- config/recipe/development/default.nix
+++ config/recipe/development/default.nix
@@ -79,18 +79,6 @@
           ));
 
         home.shellAliases = {
-          ga = "git add";
-          gc = "git commit -v";
-          gst = "git status";
-          gp = "git push";
-          gf = "git fetch --all --prune";
-          gd = "git diff";
-          ggr = "cd $(git rev-parse --show-toplevel 2>/dev/null)";
-          gl = "git pull --rebase";
-          gr = "git rebase --autostash --autosquash";
-          gsw = "git switch";
-          glp = "git pull --rebase && git push";
-
           prcm = "gh pr create --assignee @me";
           prv = "gh pr view";
           prvw = "gh pr view -w";
