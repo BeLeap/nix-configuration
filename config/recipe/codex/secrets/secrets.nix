375e8d3118d0acd15e780d30f849ad20df8c9e44
BeLeap <beleap@beleap.dev>
2026-02-24 02:03:40 +0900

    agenix: rekey context7 API secret for csjang-m3pro

--- config/recipe/codex/secrets/secrets.nix
+++ config/recipe/codex/secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   keys = import ../../../../lib/agenix/keys.nix;
 in {
   "context7-api-key.age" = {
-    publicKeys = [keys.beleap-m1air];
+    publicKeys = [keys.beleap-m1air keys.csjang-m3pro];
     armor = true;
   };
 }
