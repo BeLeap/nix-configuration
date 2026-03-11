a0749a49bf5dd00eb7c98d5a4106bf3cba1703ac
BeLeap <beleap@beleap.dev>
2026-03-11 13:50:22 +0900

    remove dummy secret

--- config/recipe/agenix/default.nix
+++ config/recipe/agenix/default.nix
@@ -1,21 +1,11 @@
 {
   metadata,
   agenix,
-}: let
-  common = import ../../../lib/agenix/common.nix {inherit metadata;};
-  age = {
-    identityPaths = common.ageIdentityPaths;
-    secrets = {
-      some-secret.file = ./secrets/some-secret.age;
-    };
-  };
-in {
+}: {
   base = [
     agenix.nixosModules.default
     {
       environment.systemPackages = [agenix.packages.${metadata.platform}.default];
-
-      inherit age;
     }
   ];
   hm = [
--- config/recipe/agenix/secrets/secrets.nix
+++
@@ -1,8 +0,0 @@
-let
-  keys = import ../../../../lib/agenix/keys.nix;
-in {
-  "some-secret.age" = {
-    publicKeys = [keys.beleap-m1air];
-    armor = true;
-  };
-}
