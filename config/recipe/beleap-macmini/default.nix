481f64521e3f7d53d71cc1c9d613e29670e2cd7d
BeLeap <beleap@beleap.dev>
2026-03-21 09:05:03 +0900

    add launchd

--- config/recipe/beleap-macmini/default.nix
+++ config/recipe/beleap-macmini/default.nix
@@ -4,4 +4,33 @@ _: {
       brews = ["zeroclaw" "ollama"];
     };
   };
+
+  hm = [
+    (_: {
+      launchd.agents = {
+        zeroclaw = {
+          enable = true;
+          config = {
+            Program = "/opt/homebrew/bin/zeroclaw";
+            ProgramArguments = ["daemon" "--host" "0.0.0.0" "--port" "80"];
+            KeepAlive = true;
+            RunAtLoad = true;
+            StandardOutPath = "/tmp/zeroclaw.log";
+            StandardErrorPath = "/tmp/zeroclaw.log";
+          };
+        };
+        ollama = {
+          enable = true;
+          config = {
+            Program = "/opt/homebrew/bin/ollama";
+            ProgramArguments = ["serve"];
+            KeepAlive = true;
+            RunAtLoad = true;
+            StandardOutPath = "/tmp/ollama.log";
+            StandardErrorPath = "/tmp/ollama.log";
+          };
+        };
+      };
+    })
+  ];
 }
