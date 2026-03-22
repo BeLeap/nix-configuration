5a9e1e8c24d0e49e61f1a8dc23a641241c240ee8
BeLeap <beleap@beleap.dev>
2026-03-23 00:02:52 +0900

    remove zeroclaw

--- config/recipe/beleap-macmini/default.nix
+++ config/recipe/beleap-macmini/default.nix
@@ -1,24 +1,13 @@
 _: {
   base = _: {
     homebrew = {
-      brews = ["zeroclaw" "ollama"];
+      brews = ["ollama"];
     };
   };
 
   hm = [
     (_: {
       launchd.agents = {
-        zeroclaw = {
-          enable = true;
-          config = {
-            Program = "/opt/homebrew/bin/zeroclaw";
-            ProgramArguments = ["daemon" "--host" "0.0.0.0" "--port" "80"];
-            KeepAlive = true;
-            RunAtLoad = true;
-            StandardOutPath = "/tmp/zeroclaw.log";
-            StandardErrorPath = "/tmp/zeroclaw.log";
-          };
-        };
         ollama = {
           enable = true;
           config = {
