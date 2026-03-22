12f78e3db66fbd7c937bf13143bb23739b65ecac
BeLeap <beleap@beleap.dev>
2026-03-23 03:50:31 +0900

    add macmini

--- config/recipe/codex/secrets/secrets.nix
+++ config/recipe/codex/secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   keys = import ../../../../lib/agenix/keys.nix;
 in {
   "context7-api-key.age" = {
-    publicKeys = [keys.beleap-m1air keys.csjang-m3pro];
+    publicKeys = [keys.beleap-m1air keys.csjang-m3pro keys.beleap-macmini];
     armor = true;
   };
 }
