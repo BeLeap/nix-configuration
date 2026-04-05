04be766a36a4899e4e04e5f356e99cbac8f0ff90
BeLeap <beleap@beleap.dev>
2026-04-06 02:00:52 +0900

    use inherit

--- lib/build-configs.nix
+++ lib/build-configs.nix
@@ -5,8 +5,10 @@
 }:
 lib.fold
 (metadata: acc: let
-  recipes = metadata.recipes;
-  system = callPackage ./mkSystem.nix {inherit metadata recipes;};
+  system = callPackage ./mkSystem.nix {
+    inherit metadata;
+    inherit (metadata) recipes;
+  };
 in {
   nixosConfigurations = acc.nixosConfigurations // system.nixosConfigurations;
   darwinConfigurations = acc.darwinConfigurations // system.darwinConfigurations;
