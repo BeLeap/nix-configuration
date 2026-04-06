3d44a02c7cc597dae13cbb5d6744cec5499b40cd
BeLeap <beleap@beleap.dev>
2026-04-06 09:53:36 +0900

    Revert "Remove work-related optional dependencies"
    This reverts commit 1e32784b6ee883ce1abfd1a7a44636d50db041bf.

--- config/recipe/development/default.nix
+++ config/recipe/development/default.nix
@@ -73,7 +73,12 @@
             ]
             ++ (lib.optionals (metadata.kind == "personal") [
               pkgs._1password-cli
-            ]);
+            ])
+            ++ (lib.optionals (metadata.kind == "work") (
+              with pkgs; [
+                unstable.jira-cli-go
+              ]
+            ));
 
           shellAliases = {
             e = "$EDITOR";
