3900a1f3423aac9366d1acc89121803300e75d78
BeLeap <beleap@beleap.dev>
2026-04-22 14:02:57 +0900

    catppuccin color theme

--- config/recipe/tmux/default.nix
+++ config/recipe/tmux/default.nix
@@ -1,12 +1,21 @@
 _: {
   hm = [
-    (_: {
+    ({pkgs, ...}: {
       programs.tmux = {
         enable = true;
 
         prefix = "C-a";
         sensibleOnTop = true;
         keyMode = "vi";
+
+        plugins = [
+          {
+            extraConfig = ''
+              set -g @catppuccin_flavor 'mocha'
+            '';
+            plugin = pkgs.tmuxPlugins.catppuccin;
+          }
+        ];
 
         extraConfig = ''
           set -g allow-passthrough all
