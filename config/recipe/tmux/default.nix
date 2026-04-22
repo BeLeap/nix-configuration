fddf27ec9d297a2b6597140ccbd73bd56d201274
BeLeap <beleap@beleap.dev>
2026-04-22 14:52:30 +0900

    add keymap

--- config/recipe/tmux/default.nix
+++ config/recipe/tmux/default.nix
@@ -24,6 +24,18 @@ _: {
         extraConfig = ''
           set -g allow-passthrough all
 
+          bind  c  new-window      -c "#{pane_current_path}"
+          bind  %  split-window -h -c "#{pane_current_path}"
+          bind '"' split-window -v -c "#{pane_current_path}"
+
+          bind-key -T copy-mode-vi v send-keys -X begin-selection
+          bind-key -T copy-mode-vi y send-keys -X copy-selection
+
+          bind h select-pane -L
+          bind j select-pane -D
+          bind k select-pane -U
+          bind l select-pane -R
+
           bind r source-file ~/.config/tmux/tmux.conf
         '';
       };
