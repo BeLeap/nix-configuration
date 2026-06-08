74757c53e7f59254d518e473e8b5cef9db5b77ff
BeLeap <beleap@beleap.dev>
2026-06-08 11:03:01 +0900

    add syncthing devices

--- config/recipe/syncthing/default.nix
+++ config/recipe/syncthing/default.nix
@@ -5,6 +5,14 @@ _: {
         enable = true;
 
         settings = {
+          devices = {
+            beleap-m1air = {
+              id = "GDUYEXA-YGTOHI2-P6HTI3P-YRIKT6X-OUC4OFR-3FDRD3R-NQVSDKZ-BHOZXA3";
+            };
+            beleap-s24 = {
+              id = "G2ERWU6-3M4MXCI-EM3ZS5W-TX3A4I5-ERHRC7V-5FJG75F-7RVNKHF-JACTWA4";
+            };
+          };
           folders = {
             "${config.home.homeDirectory}/Sync" = {
               id = "sync";
