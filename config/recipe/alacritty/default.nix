77db84b2ce274d83a34390b7b092ff9e3745258f
BeLeap <beleap@beleap.dev>
2026-06-10 11:06:37 +0900

    some decorative settings

--- config/recipe/alacritty/default.nix
+++ config/recipe/alacritty/default.nix
@@ -14,6 +14,12 @@ _: {
             normal = {family = "Hack Nerd Font Mono";};
             size = 16;
           };
+          window = {
+            decorations = "None";
+            padding = {
+              x = 8;
+            };
+          };
           terminal = {
             shell = {
               program = "${lib.getExe pkgs.zsh}";
