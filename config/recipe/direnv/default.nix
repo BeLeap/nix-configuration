759a880f52c4f8d19385081c91e923b09f0146f6
BeLeap <beleap@beleap.dev>
2026-06-15 00:04:48 +0900

    fix statix error

--- config/recipe/direnv/default.nix
+++ config/recipe/direnv/default.nix
@@ -7,21 +7,23 @@
     direnv-instant.homeModules.direnv-instant
     (
       {pkgs, ...}: {
-        programs.direnv = {
-          enable = true;
-          package = pkgs.direnv;
+        programs = {
+          direnv = {
+            enable = true;
+            package = pkgs.direnv;
 
-          enableZshIntegration = true;
-          enableBashIntegration = true;
+            enableZshIntegration = true;
+            enableBashIntegration = true;
 
-          silent = true;
-          config = {
-            load_dotenv = true;
-            warn_timeout = "1m";
+            silent = true;
+            config = {
+              load_dotenv = true;
+              warn_timeout = "1m";
+            };
           };
+          direnv-overlay.enable = true;
+          direnv-instant.enable = true;
         };
-        programs.direnv-overlay.enable = true;
-        programs.direnv-instant.enable = true;
       }
     )
   ];
