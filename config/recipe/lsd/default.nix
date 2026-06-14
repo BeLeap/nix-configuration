af8546e7633fbd0b6657b1c001ffc2442271931d
BeLeap <beleap@beleap.dev>
2026-06-15 02:42:08 +0900

    remove alias

--- config/recipe/lsd/default.nix
+++ config/recipe/lsd/default.nix
@@ -5,12 +5,6 @@ _: {
         programs.lsd = {
           enable = true;
         };
-        home.shellAliases = {
-          ls = "lsd";
-          ll = "lsd -l";
-          lla = "lsd -la";
-          lt = "lsd -lt";
-        };
       }
     )
   ];
