84b9f656b8e53c86bfc94e768e4b143023d5d189
BeLeap <beleap@beleap.dev>
2026-06-28 14:32:43 +0900

    add cache

--- config/recipe/nix/default.nix
+++ config/recipe/nix/default.nix
@@ -9,6 +9,17 @@ _: {
           experimental-features = "nix-command flakes";
           min-free = "1M";
           accept-flake-config = true;
+
+          extra-substituters = [
+            "https://nix-community.cachix.org"
+            "https://cache.numtide.com"
+            "https://beleap-nix-overlay.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+            "beleap-nix-overlay.cachix.org-1:SJdfYkwp5t+YoeraOtUcqyKIxvnWQ1GdiNoC21F+En0="
+          ];
         };
 
         gc = {};
