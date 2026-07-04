73e522077b652c3e59d39e8a877824089f276775
BeLeap <beleap@beleap.dev>
2026-07-05 03:38:25 +0900

    add beleap-ipad

--- config/recipe/syncthing/default.nix
+++ config/recipe/syncthing/default.nix
@@ -12,6 +12,9 @@ _: {
             beleap-s24 = {
               id = "G2ERWU6-3M4MXCI-EM3ZS5W-TX3A4I5-ERHRC7V-5FJG75F-7RVNKHF-JACTWA4";
             };
+            beleap-ipad = {
+              id = "QTG36Q4-TRUP4O2-HVZTTKA-Z2SWAA7-4AFTWMS-3QVEMSY-ITB42OY-ZAIAYQF";
+            };
           };
           folders = {
             "${config.home.homeDirectory}/Sync" = {
