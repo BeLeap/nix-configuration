78a44dddb38ab7557cc1fa5895e0ff5288d1a504
BeLeap <beleap@beleap.dev>
2026-07-13 00:32:07 +0900

    use wezterm-dmg from beleap-overlay

--- config/recipe/wezterm/default.nix
+++ config/recipe/wezterm/default.nix
@@ -5,40 +5,6 @@ _: {
       pkgs,
       ...
     }: let
-      weztermDmg = pkgs.stdenv.mkDerivation {
-        pname = "wezterm";
-        version = "20240203-110809-5046fc22";
-
-        src = pkgs.fetchurl {
-          url = "https://github.com/wezterm/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-macos-20240203-110809-5046fc22.zip";
-          hash = "sha256-53OIytVfLp2pWiIKiSBqbFj4ZYdKYpt8PqPBYvVpIiQ=";
-        };
-        sourceRoot = ".";
-
-        nativeBuildInputs = [pkgs.unzip];
-
-        unpackPhase = ''
-          runHook preUnpack
-          unzip "$src"
-          runHook postUnpack
-        '';
-
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out/Applications
-          mkdir -p $out/bin
-          # DMG 안의 모든 .app을 복사 (하나만 있으면 하나만 복사됨)
-          cp -R WezTerm-macos-20240203-110809-5046fc22/WezTerm.app $out/Applications/
-          for bin in wezterm wezterm-gui wezterm-mux-server; do
-            ln -s $out/Applications/WezTerm.app/Contents/MacOS/$bin $out/bin/$bin
-          done
-          runHook postInstall
-        '';
-
-        meta.platforms = [
-          "aarch64-darwin"
-        ];
-      };
       weztermConfig = pkgs.writeText "wezterm.lua" (
         builtins.replaceStrings ["@zsh@"] ["${lib.getExe pkgs.zsh}"] (builtins.readFile ./wezterm.lua)
       );
@@ -46,7 +12,7 @@ _: {
       home.packages = [
         (
           if pkgs.stdenv.isDarwin
-          then weztermDmg
+          then pkgs.wezterm-dmg
           else pkgs.wezterm
         )
       ];
