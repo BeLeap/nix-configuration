e947057808047909bda1adf8d91692775d56b0d6
BeLeap <beleap@beleap.dev>
2026-08-23 11:48:30 +0900

    add lnd

--- config/recipe/overlay/pkgs/beleap-utils/default.nix
+++ config/recipe/overlay/pkgs/beleap-utils/default.nix
@@ -12,7 +12,7 @@ in
 
     postBuild = ''
       for bin in $out/bin/*; do
-        wrapProgram "$bin" --prefix PATH : ${lib.makeBinPath [pkgs.jq]}
+        wrapProgram "$bin" --prefix PATH : ${lib.makeBinPath (with pkgs; [jq fd])}
       done
     '';
   }
