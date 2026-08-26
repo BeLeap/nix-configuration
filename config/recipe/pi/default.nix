7c8010701480e5dc55d55a74496e44c87ea2ee36
Changseo Jang <beleap@beleap.dev>
2026-08-26 14:50:41 +0900

    Remove npm:@hk_net/pi-thinking-command
    Remove npm:@hk_net/pi-thinking-command from providers list.

--- config/recipe/pi/default.nix
+++ config/recipe/pi/default.nix
@@ -73,7 +73,6 @@
                 "npm:pi-web-access"
                 "https://github.com/tmustier/pi-queue-steer"
                 "npm:pi-title-renamer"
-                "npm:@hk_net/pi-thinking-command"
               ];
             };
             ".pi/agent/models.json".text = builtins.toJSON {
