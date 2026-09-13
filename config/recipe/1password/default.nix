1ab35b7ae085dc7c7b6c2b9efa19afbe92d9d1b0
BeLeap <beleap@beleap.dev>
2026-09-13 15:23:32 +0900

    1password ssh agent

--- config/recipe/1password/default.nix
+++ config/recipe/1password/default.nix
@@ -15,6 +15,11 @@ _: {
         homebrew.casks = ["1password"];
       })
     ];
+    home = [
+      (_: {
+        programs.ssh.settings."*".IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+      })
+    ];
   };
 
   nixos = {
@@ -23,6 +28,11 @@ _: {
         programs._1password-gui = {
           enable = true;
         };
+      })
+    ];
+    home = [
+      (_: {
+        programs.ssh.settings."*".IdentityAgent = "~/.1password/agent.sock";
       })
     ];
   };
